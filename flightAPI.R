dc81064608b69a9944709aea19987e418a97a7bd
Hashim Alawami <hashim.alawami@gmail.com>
2016-08-16 21:53:35 +0200

    Parsed the data from JSON

--- flightAPI.R
+++ flightAPI.R
@@ -1,5 +1,53 @@
 library(googleflights)
+library(jsonlite)
+library(readr)
 set_apikey("AIzaSyBcyOApaEifsmwW1zIaKBIVfw16gGGV4Z8")
 #results <- search(origin="CPT",dest = "JNB", startDate = Sys.Date()+1, returnDate = Sys.Date() + 8)
-library(readr)
-library(jsonlite)
+
+
+results.json <- toJSON(results)
+
+n <- 200
+
+airlines <- sapply(1:length(results$trips$data$carrier), function(x){
+  results$trips$data$carrier[[x]]$name
+})
+
+
+flights_price_roundtrip <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$pricing[[1]]$saleTotal
+})
+
+flights_carrier_outbound <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$slice[[1]]$segment[[1]]$flight$carrier
+})
+
+flights_code_outbound <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$slice[[1]]$segment[[1]]$flight$number
+})
+
+flights_deptime_outbound <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$slice[[1]]$segment[[1]]$leg[[1]]$departureTime
+})
+
+
+flights_carrier_inbound <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$slice[[2]]$segment[[1]]$flight$carrier
+})
+
+flights_code_inbound <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$slice[[2]]$segment[[1]]$flight$number
+})
+
+flights_deptime_inbound <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$slice[[2]]$segment[[1]]$leg[[1]]$departureTime
+})
+
+fareCalculation <- sapply(1:n, function(x){
+  results$trips$tripOption[[x]]$pricing[[1]]$fareCalculation
+})
+
+
+
+
+
