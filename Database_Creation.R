4851f214fa3d38a133ef95b72c9efed2655c4df9
Andrew Musk <andrewdmusk@gmail.com>
2016-08-17 16:01:21 +0200

    fixed database script

--- Database_Creation.R
+++ Database_Creation.R
@@ -5,9 +5,7 @@ dbSendQuery(conn = db,
             "CREATE TABLE tb_flights
             (flight_id   INTEGER PRIMARY KEY AUTOINCREMENT,
             flight_code   VARCHAR(128),
-            carrier   VARCHAR(128),
-            depart   DATETIME,
-            query   DATETIME
+            carrier   VARCHAR(128)
             )")
 
 dbSendQuery(conn = db,
@@ -19,3 +17,9 @@ dbSendQuery(conn = db,
             departure DATETIME,
             FOREIGN KEY (flight_id) REFERENCES flights(flight_id)
             )")
+
+#Inserting into the table flights
+
+query <- sprintf("('%s','%s')", unique(results.db$flights_code_outbound),unique(results.db$flights_carrier_outbound))
+query <- paste(query,collapse = ",")
+dbSendQuery(db,str_c("INSERT INTO tb_flights(flight_code,carrier) VALUES ",query))
