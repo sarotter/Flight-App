ea0948ad2b5b807ffdccfb8df05169f0e5b0e281
Andrew Musk <andrewdmusk@gmail.com>
2016-08-17 20:01:40 +0200

    date working

--- flightAPI.R
+++ flightAPI.R
@@ -117,7 +117,7 @@ priceQuery <- paste(priceQuery,collapse = ",")
 
 trypriceQuery <- "(110,convert(datetime,'2016-08-17 19:10:48',120),convert(datetime,'2016-08-18 21:05:00',120),'SA2133')"
 
-dbSendQuery(db,"INSERT INTO tb_flights(price,query,departure,flight_code) VALUES(110,convert(datetime,'2016-08-17 19:10:48',120),convert(datetime,'2016-08-18 21:05:00',120),'SA2133')")
+dbSendQuery(db,"INSERT INTO tb_flights(price,query,departure,flight_code) VALUES(110,'2016-08-17 19:10:48','2016-08-18 21:05:00','SA2133')")
 
 
 
